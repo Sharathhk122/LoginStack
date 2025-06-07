9221bab0e99b3717979b0eb3f4a45619bc32bde1
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-06-07 11:40:50 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api/auth',
+  baseURL: 'https://loginstack.onrender.com/api/auth',
 });
 
 export const signup = (data) => API.post('/signup', data);
 export const verifyOtp = (data) => API.post('/verify-otp', data);
 export const login = (data) => API.post('/login', data);
+
